20ec1b18f30ba19d22a78a708a9c804e5501f0b4
@Danisdnk <danisdnk@gmail.com>
2020-09-09 00:32:16 -0300

    Leer una cadena de caracteres e imprimirla centrada en pantalla.

---
+++ TP4/4.2.py
@@ -0,0 +1,14 @@
+# Leer una cadena de caracteres e imprimirla centrada en pantalla.
+# Suponer que la misma tiene 80 columnas.
+
+def centrar(cadena):
+    extremos=(80-len(cadena))//2
+    relleno=" "*extremos
+    centrada ="|"+relleno+cadena+relleno+"|"
+    return centrada
+
+
+cadena = input("Ingrese la cadena que quiere cortar")
+centrado = centrar(cadena)
+print(centrado)
+# Yo nunca me pierdo...son los demás los que no me encuentran.
